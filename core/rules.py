d4842ef0fdb631d17f5745c28d8850958d9a6403
daryloye <daryloye@gmail.com>
2025-09-29 00:44:54 +0800

    v2

--- core/rules.py
+++ core/rules.py
@@ -1,5 +1,6 @@
 from config import Config
 from core.move import Move
+from functools import lru_cache
 
 
 class Rules:
@@ -24,21 +25,47 @@ class Rules:
         return False
 
     
-    def evaluate(self, stones, move):
+    def evaluate(self, stones, ai_colour, move):
         if move == None:
             return 0
         if self.checkWin(stones, move):
-            return 1
-        
-        max_len = 0
+            return float('inf') if move.colour == ai_colour else -float('inf')
+
+        scores = {
+            1: 1,
+            2: 10,
+            3: 100,
+            4: 1000,
+        }
+
+        cache = {}
+        def get_row_length_cached(move):
+            key = (move.tile, move.colour)
+            if key not in cache:
+                cache[key] = self._getRowLength(stones, move)
+            return cache[key]
+
+        max_ai = 0
+        max_opp = 0
         for tile, colour in stones.map.items():
-            if colour == move.colour:
-                temp_move = Move(tile, colour)
-                max_len = max(max_len, self._getRowLength(stones, temp_move))
+            temp_move = Move(tile, colour)
+            length = get_row_length_cached(temp_move)
+            score = scores.get(length, 10 ** (length - 1))
+            # length = self._getRowLength(stones, temp_move)
+            if colour == ai_colour:
+                max_ai = max(max_ai, score)
+            else:
+                if length >= 4:
+                    return -1e9
+                if length == 3:
+                    return -1e6
+                max_opp = max(max_opp, score)
         
-        return max_len / 5
+        print(max_ai, max_opp)
+        return max_ai - max_opp * 2
 
 
+    # TODO: cache this
     def _getRowLength(self, stones, move):
         directions = [
             (0, 1),
